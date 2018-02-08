7e4b4cef9a018ee49b0f3fd9f5d2bec41785df97
sleggg <35469625+sleggg@users.noreply.github.com>
2018-02-08 16:52:58 +0200

    Add files via upload

---
+++ delfi-comments.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           changeurl test
+// @namespace      http://*.delfi.ee/
+// @description    Redirecting URLs to see unregistered comments 
+// @include        http://*.delfi.ee/*
+// ==/UserScript==
+
+var links,thisLink;
+links = document.evaluate("//a[@href]",
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null);
+for (var i=0;i<links.snapshotLength;i++) {
+    var thisLink = links.snapshotItem(i);
+
+    thisLink.href = thisLink.href.replace('&reg=1',
+                                          '&reg=0');
+}
